ca8676b21fe3ab27b6f86fd98d3a3f0aab8d88a8
ronald-so <ronald.so1996@gmail.com>
2019-07-14 01:15:56 -0400

    Added CLI, exit functionality

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 // Modules
 const fs = require('fs')
 const ytdl = require('ytdl-core')
+const inquirer = require('inquirer')
 
 // Download Directory
 const dir = 'videos/'
 
-ytdl('https://www.youtube.com/watch?v=dQw4w9WgXcQ').pipe(fs.createWriteStream(dir + 'video.mp4'));
+const download = async () => {
+    inquirer.prompt(
+    {type: 'input', name: 'url', message: 'Enter video URL, or type "exit" to close: '}
+).then(answers => {
+    if (answers.url.toLowerCase() == 'exit') {return} else {
+    ytdl(answers.url).pipe(fs.createWriteStream(dir + 'video.mp4'));
+    return download()
+    }
+})
+}
+
+download();
