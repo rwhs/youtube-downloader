6fa80c7f99d95224a78f696ac89d64b684cf57b2
ronald-so <ronald.so1996@gmail.com>
2019-07-15 00:30:22 -0400

    Added React

--- server/main.js
+++ server/main.js
@@ -1,20 +1,25 @@
 // Modules
 const fs = require('fs')
+const express = require('express')
 const ytdl = require('ytdl-core')
 const inquirer = require('inquirer')
 
 // Download Directory
 const dir = 'videos/'
 
-const download = async () => {
+// Download Function
+var download = async () => {
     inquirer.prompt(
     {type: 'input', name: 'url', message: 'Enter video URL, or type "exit" to close: '}
 ).then(answers => {
-    if (answers.url.toLowerCase() == 'exit') {return} else {
-    ytdl(answers.url).pipe(fs.createWriteStream(dir + 'video.mp4'));
-    return download()
+    // Exit
+    if (answers.url.toLowerCase() == 'exit') { return } 
+    else {
+        ytdl(answers.url).pipe(fs.createWriteStream(dir + 'video.mp4'));
+        return download()
     }
 })
 }
 
+// Call function
 download();
