e6152b37e1d553834a0f99fa9afc6f3a3fc48341
ronald-so <ronald.so1996@gmail.com>
2019-07-19 16:46:28 -0400

    downloading works now, need to fix components

--- client/src/components/DownloadButton.jsx
+++ client/src/components/DownloadButton.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import axios from 'axios';
+const fileDownload = require('js-file-download');
 
 class DownloadButton extends React.Component {
 
+  handleDownload() {
+    axios.get('http://localhost:4000/download', {
+      responseType: 'blob',
+    })
+    .then((res) => {
+      fileDownload(res.data, 'video.mp4')
+    })
+    console.log('Requested download');
+  }
+
   render() {
     return (
-      <button className="button is-link" disabled={!this.props.isReady}>Download</button>
+      <button className="button is-link" disabled={!this.props.isReady} onClick={this.handleDownload}>Download</button>
     )
   }
 }
