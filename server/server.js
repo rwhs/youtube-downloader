4ee6bc7be7f5bbc629881e2a7b935d8123ebb722
ronald-so <ronald.so1996@gmail.com>
2019-07-21 00:29:34 -0400

    clean up code

--- server/server.js
+++ server/server.js
@@ -5,26 +5,21 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const ytdl = require('ytdl-core')
-const mime = require('mime');
-
 
 // Download Directory
 const dir = 'videos/'
 const PORT = 4000;
 
-
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-
 
 // Main
 app.get('/', (req, res) => {
     res.send('Home page');
 });
 
-
-// Download request
+// Add video POST request
 app.post('/test', (req, res) => {
     console.log(req.body.url);
     if (ytdl.validateURL(req.body.url)) { // Validate URL
@@ -32,24 +27,13 @@ app.post('/test', (req, res) => {
 
         // Download
         dl.pipe(fs.createWriteStream(dir + 'video.mp4'));
+
         // Download button not ready until finished
-        // res.send({
-        //     downloadIsReady: ''
-        // });
         dl.on('finish', () => {
             console.log('File download finished'); // Log when download finished
-
             res.send({
                 downloadIsReady: 'true',
             });
-            // res.end()
-            // let file = dir + 'video.mp4';
-            // let filetype = mime.getType(file);
-
-            // res.setHeader('Content-type', filetype);
-            // res.setHeader('Content-disposition', 'attachment; filename=download.mp4');
-            // res.download('./videos/video.mp4');
-            // fs.createReadStream('./videos/video.mp4').pipe(res);
         })
     } else {
         console.log('Invalid URL');
@@ -59,6 +43,7 @@ app.post('/test', (req, res) => {
     }
 })
 
+// Download video GET request
 app.get('/download', (req, res) => {
     res.download('./videos/video.mp4', 'video.mp4', function (err) {
         console.log('Transfer complete');
